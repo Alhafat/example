6ba868f1982c38b72bd81955a706d1d18ba7c6e1
Alhafat <vladimir-lukin@inbox.ru>
2023-01-24 18:07:40 +0300

    example_011

---
+++ example_011/Program.cs
@@ -0,0 +1,11 @@
+﻿string Method4(int count, string c)
+{
+    string result=String.Empty;
+    for (int i=0; i < count; i++)
+    {
+        result=result+c;
+    }
+    return result;
+}
+string res=Method4(10,"asdf"); //количество раз, "что выводит"
+Console.WriteLine(res);
