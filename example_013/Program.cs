d2488f22c0e88f352c94f0ee479c8794a5119392
Alhafat <vladimir-lukin@inbox.ru>
2023-01-24 19:37:18 +0300

    examples_013

---
+++ example_013/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] list={1,3,2,5,4};
+
+void PrintArray(int[] list)
+{
+    int count=list.Length;
+
+    for (int i=0; i<count; i++)
+    {
+        Console.Write($"{list[i]}");
+    }
+    Console.WriteLine();
+}
+PrintArray(list);
+
+void SelectionSort(int[] list)
+{
+    for (int i=0; i<list.Length-1; i++)
+    {
+        int minPosition=i;
+        for (int j=i+1; j<list.Length; j++)
+        {
+            if (list[j]<list[minPosition]) 
+            {
+                minPosition=j;
+            }
+        }
+        int temporary=list[i];
+        list[i]=list[minPosition];
+        list[minPosition]=temporary;
+
+    }
+}
+PrintArray(list);
+SelectionSort(list);
+PrintArray(list);
