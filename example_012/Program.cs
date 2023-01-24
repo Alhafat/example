0661d38f3d48d3caa6e067439cb4b44dbdded9f7
Alhafat <vladimir-lukin@inbox.ru>
2023-01-24 19:20:50 +0300

    example_012

---
+++ example_012/Program.cs
@@ -0,0 +1,18 @@
+﻿string text="- Я, придя домой, смотрю дуракцие лекции," 
++"в которых объяснения мне не понятны в 90% случаях." 
++"Вывод - за что заплачены деньги?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result=String.Empty;
+
+    int length=text.Length;
+    for (int i=0; i<length; i++)
+    {
+        if(text[i]==oldValue) result=result+$"{newValue}";
+        else result=result+$"{text[i]}";
+    }
+    return result;
+}
+string newText=Replace(text, ' ','|');
+Console.WriteLine(newText);
